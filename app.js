ccc39f3a6c4c76874580d4697acd6526a598a987
piotr <pegaz171@o2.pl>
2022-03-31 21:21:22 +0200

    keroku deployment - port

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.url)
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
