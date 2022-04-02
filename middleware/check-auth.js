ba8ac134692a888ff19aa854257e14e76240b415
piotr <pegaz171@o2.pl>
2022-04-02 11:07:16 +0200

    deployment...

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -11,6 +11,8 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
 
+    console.log({ token });
+
     if (!token) {
       throw new Error("Authentication failed");
     }
